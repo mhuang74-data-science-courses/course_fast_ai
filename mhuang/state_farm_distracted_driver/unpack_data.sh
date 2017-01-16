0ed4626597d2b5b08f1c0067d726b7704fc269fd
Michael S. Huang <mhuang@marinsoftware.com>
2017-01-16 08:53:09 +0800

    separate validation set from train; create full sampling set with train, valid, and test

--- mhuang/state_farm_distracted_driver/unpack_data.sh
+++ mhuang/state_farm_distracted_driver/unpack_data.sh
@@ -3,38 +3,36 @@
 mkdir data
 cd data
 unzip -q ~/downloads/data/kaggle_state_farm_distracted_driver/imgs.zip
+unzip -q ~/downloads/data/kaggle_state_farm_distracted_driver/driver_imgs_list.csv.zip
+
+##### start VALIDATION SET #####
 
 # data unpacts into 'train' directory, which we will separate images from drivers into 'valid' directory
 rm -rf valid
+mkdir valid
+# cat ../driver_imgs_list.csv | awk -F',' '{print $1}' | uniq -c | sort -nr
+# select drivers p081, p012, p064 to be in validation set
 
+cd train
+# first copy _all_  selected valid imgs over to 'valid' directory
+egrep "p081|p012|p064" ../driver_imgs_list.csv | awk -F',' '{print $2 "/" $3;}' | xargs cp --parents -p -t ../valid
+# then remove them from 'train' directory
+egrep "p081|p012|p064" ../driver_imgs_list.csv | awk -F',' '{print $2 "/" $3;}' | xargs rm
+
+cd ..
+##### end VALIDATION SET #####
+
+
+##### start SAMPLE SET #####
 # now copy a subset of 'train', 'valid' and 'test' into sample directory
 rm -rf sample
+mkdir sample
+
+# train
+find train -name "*.jpg" | shuf -n1000 | xargs cp -p --parent -t sample 
+# valid
+find valid -name "*.jpg" | shuf -n200 | xargs cp -p --parent -t sample 
+# test
+find test -name "*.jpg" | shuf -n50 | xargs cp -p --parent -t sample 
 
 
-# sample/train
-mkdir -p sample/train/c0
-mkdir -p sample/train/c1
-mkdir -p sample/train/c2
-mkdir -p sample/train/c3
-mkdir -p sample/train/c4
-mkdir -p sample/train/c5
-mkdir -p sample/train/c6
-mkdir -p sample/train/c7
-mkdir -p sample/train/c8
-mkdir -p sample/train/c9
-
-# do shuffle-copy independently for each of the 10 categories
-shuf -zn32 -e train/c0/*.jpg | xargs -0 cp -t sample/train/c0
-shuf -zn32 -e train/c1/*.jpg | xargs -0 cp -t sample/train/c1
-shuf -zn32 -e train/c2/*.jpg | xargs -0 cp -t sample/train/c2
-shuf -zn32 -e train/c3/*.jpg | xargs -0 cp -t sample/train/c3
-shuf -zn32 -e train/c4/*.jpg | xargs -0 cp -t sample/train/c4
-shuf -zn32 -e train/c5/*.jpg | xargs -0 cp -t sample/train/c5
-shuf -zn32 -e train/c6/*.jpg | xargs -0 cp -t sample/train/c6
-shuf -zn32 -e train/c7/*.jpg | xargs -0 cp -t sample/train/c7
-shuf -zn32 -e train/c8/*.jpg | xargs -0 cp -t sample/train/c8
-shuf -zn32 -e train/c9/*.jpg | xargs -0 cp -t sample/train/c9
-
-# sample/test
-mkdir -p sample/test
-shuf -zn50 -e test/*.jpg | xargs -0 cp -t sample/test
