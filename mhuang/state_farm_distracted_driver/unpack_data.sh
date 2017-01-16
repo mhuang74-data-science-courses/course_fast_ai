6d59c0fc478115d5e41101a6f0a10adf8edcfe9d
Michael S. Huang <mhuang@marinsoftware.com>
2017-01-16 10:24:30 +0800

    move all test imgs under 'unknown' category directory

--- mhuang/state_farm_distracted_driver/unpack_data.sh
+++ mhuang/state_farm_distracted_driver/unpack_data.sh
@@ -3,14 +3,23 @@
 mkdir data
 cd data
 unzip -q ~/downloads/data/kaggle_state_farm_distracted_driver/imgs.zip
-unzip -q ~/downloads/data/kaggle_state_farm_distracted_driver/driver_imgs_list.csv.zip
+
+##### fix up TEST directory #####
+cd test
+mkdir unknown
+mv * unknown
+cd ..
+##### fix up TEST directory #####
+
+
 
 ##### start VALIDATION SET #####
 
 # data unpacts into 'train' directory, which we will separate images from drivers into 'valid' directory
 rm -rf valid
 mkdir valid
-# cat ../driver_imgs_list.csv | awk -F',' '{print $1}' | uniq -c | sort -nr
+unzip -q ~/downloads/data/kaggle_state_farm_distracted_driver/driver_imgs_list.csv.zip
+# cat driver_imgs_list.csv | awk -F',' '{print $1}' | uniq -c | sort -nr
 # select drivers p081, p012, p064 to be in validation set
 
 cd train
@@ -20,10 +29,12 @@ egrep "p081|p012|p064" ../driver_imgs_list.csv | awk -F',' '{print $2 "/" $3;}' 
 egrep "p081|p012|p064" ../driver_imgs_list.csv | awk -F',' '{print $2 "/" $3;}' | xargs rm
 
 cd ..
+
 ##### end VALIDATION SET #####
 
 
 ##### start SAMPLE SET #####
+
 # now copy a subset of 'train', 'valid' and 'test' into sample directory
 rm -rf sample
 mkdir sample
@@ -34,5 +45,8 @@ find train -name "*.jpg" | shuf -n1000 | xargs cp -p --parent -t sample
 find valid -name "*.jpg" | shuf -n200 | xargs cp -p --parent -t sample 
 # test
 find test -name "*.jpg" | shuf -n50 | xargs cp -p --parent -t sample 
+
+##### start SAMPLE SET #####
+
 
 
