86388bf1fc7afb8c3642154cec4a07225ada6991
mhuang74 <mhuang74@gmail.com>
2017-07-09 15:34:42 +0000

    fix up unpack script

--- mhuang/state_farm_distracted_driver/unpack_data.sh
+++ mhuang/state_farm_distracted_driver/unpack_data.sh
@@ -1,16 +1,27 @@
 #unpacks Kaggle State Farm imgs.zip to directories suitable for Keras 
 # this script also cp subset of files from 'train' and 'test' directories to create 'sample' directory
+KAGGLE_DATA_PATH="$HOME/downloads/data/kaggle_state_farm_distracted_driver"
 mkdir -p data data/models
 cd data
-unzip -q ~/downloads/data/kaggle_state_farm_distracted_driver/imgs.zip
-unzip -q ~/downloads/data/kaggle_state_farm_distracted_driver/driver_imgs_list.csv.zip
+
+unzip -q $KAGGLE_DATA_PATH/imgs.zip
+
+##### fix up TEST directory #####
+cd test
+mkdir unknown
+mv * unknown
+cd ..
+##### fix up TEST directory #####
+
+
 
 ##### start VALIDATION SET #####
 
 # data unpacts into 'train' directory, which we will separate images from drivers into 'valid' directory
 rm -rf valid
 mkdir valid
-# cat ../driver_imgs_list.csv | awk -F',' '{print $1}' | uniq -c | sort -nr
+unzip -q $KAGGLE_DATA_PATH/driver_imgs_list.csv.zip
+# cat driver_imgs_list.csv | awk -F',' '{print $1}' | uniq -c | sort -nr
 # select drivers p081, p012, p064 to be in validation set
 
 cd train
@@ -20,19 +31,24 @@ egrep "p081|p012|p064" ../driver_imgs_list.csv | awk -F',' '{print $2 "/" $3;}' 
 egrep "p081|p012|p064" ../driver_imgs_list.csv | awk -F',' '{print $2 "/" $3;}' | xargs rm
 
 cd ..
+
 ##### end VALIDATION SET #####
 
 
 ##### start SAMPLE SET #####
+
 # now copy a subset of 'train', 'valid' and 'test' into sample directory
 rm -rf sample
 mkdir -p sample sample/models
 
 # train
-find train -name "*.jpg" | shuf -n2000 | xargs cp -p --parent -t sample 
+find train -name "*.jpg" | shuf -n500 | xargs cp -p --parent -t sample 
 # valid
-find valid -name "*.jpg" | shuf -n400 | xargs cp -p --parent -t sample 
+find valid -name "*.jpg" | shuf -n100 | xargs cp -p --parent -t sample 
 # test
-find test -name "*.jpg" | shuf -n50 | xargs cp -p --parent -t sample 
+find test -name "*.jpg" | shuf -n25 | xargs cp -p --parent -t sample 
+
+##### end SAMPLE SET #####
+
 
 
